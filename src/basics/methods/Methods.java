b86ed39a113af7df414e03d04560d509a270b2ca
Chahak <chahak2003jain@gmail.com>
2026-01-15 22:00:25 +0530

    methods in java

---
+++ src/basics/methods/Methods.java
@@ -0,0 +1,25 @@
+package basics.methods;
+
+public class Methods {
+    public static void main(String args[]){
+        /*When a method is called, Java uses an internal structure known
+         as the call stack to manage execution, variables, and return addresses.
+         */
+
+        /* Call Stack : a data structure used by the program during runtime to manage method calls and local variables. It operates in a Last-In-First-Out (LIFO) manner,
+         meaning the last method called is the first one to complete and exi
+         */
+
+        /*How are methods executed?
+        When a method is called,
+            1. A new stack frame is added to the call stack to store method details
+            2. The method runs its code.
+            3. After execution, the stack frame is removed, and control goes back to the calling method
+
+         */
+
+        //Java automatically manages the call stack using the Java Virtual Machine (JVM).
+
+        //TODO: Types of method
+    }
+}
