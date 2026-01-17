3d83fb4527b7edafde13f6b180ec13e5539975e5
Chahak <chahak2003jain@gmail.com>
2026-01-17 23:34:39 +0530

    Strings in java

---
+++ src/basics/stringsinjava/JavaStrings.java
@@ -0,0 +1,9 @@
+package basics.stringsinjava;
+
+public class JavaStrings {
+    //There are two ways to create strings in java
+    //1. Static literal (Static memory)
+    String str = "Chahak";
+    //2.Using heap memory
+    String s = new String("ChahakJain");
+}
